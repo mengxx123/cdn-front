dd959d4805970e0a14d60c984694782d1773edc2
gitcjh <1418503647@.qq.com>
2017-12-15 16:25:19 +0800

    backup

---
+++ src/components/header/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="page-header">
+        <div class="container">
+            <div>云设 CDN</div>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            }
+        },
+        mounted() {
+        },
+        methods: {
+        }
+    }
+</script>
